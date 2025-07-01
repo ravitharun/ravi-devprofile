8358526226f7b28281d8ed63a6bd8ff285444e5b
Ravi Tharun <tharunravi672@gmail.com>
2025-07-01 17:37:06 +0530

    Fix: removed trailing slash in CORS origin

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,10 @@ require('dotenv').config(); // Load .env
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors({
+  origin: "https://raviprotifilo.netlify.app", // ✅ your frontend domain
+  methods: ["POST", "GET"]
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
