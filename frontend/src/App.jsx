27f774ead9307eda340ebbbd1bf647edc34b5422
Ravi Tharun <tharunravi672@gmail.com>
2025-07-02 18:19:11 +0530

    Added the Seo tage and also updated the code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
 
 import About from "./Pages/About";
+import { Helmet } from "react-helmet";
+
 function App() {
   return (
     <>
+      <Helmet>
+        <meta name="google-site-verification" content="google64e9e9010771d280.html" />
+      </Helmet>
+       <Helmet>
+        <title>Tharun Ravi | MERN Stack Developer</title>
+        <meta name="google-site-verification" content="google64e9e9010771d280.html" />
+        <meta name="description" content="Explore MERN Stack projects and professional journey of Tharun Ravi." />
+        <meta property="og:title" content="Tharun Ravi Protifilo" />
+        <meta property="og:image" content="https://ravi-tharun-protifilo.netlify.app/images/protifilo-preview.png" />
+        <meta property="og:url" content="https://ravi-tharun-protifilo.netlify.app" />
+        <meta property="og:type" content="website" />
+      </Helmet>
+
     <About/>
     </>
   );
